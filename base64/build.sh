db587837c19b39fc0dd309710fe8abc2a0211467
Gabriel Jauffret <gabjauf@hotmail.fr>
2021-04-17 13:38:48 +0200

    Add some testing on OS and docker

--- base64/build.sh
+++ base64/build.sh
@@ -4,22 +4,38 @@ set -e
 
 cd ../common/libnotify; make; cd -
 
-crystal build test.cr --release -o base64_cr --no-debug
-go build -o base64_go test.go
-gccgo -O3 -g -o base64_go_gccgo test.go
-g++ -O3 -o base64_cpp test.cpp -lcrypto -I../common/libnotify -L../common/libnotify -lnotify
-gcc -O3 -o base64_c test.c -I../common/libnotify -L../common/libnotify -lnotify
-scalac test.scala
-javac Base64Java.java
-kotlinc Test.kt -include-runtime -jvm-target 13 -d Test-kt.jar
-dmd -ofbase64_d -O -release -inline test.d
-gdc -o base64_d_gdc -O3 -frelease -finline test.d
-ldc2 -ofbase64_d_ldc -O5 -release test.d
-nim c -o:base64_nim_gcc -d:danger --cc:gcc --verbosity:0 test.nim
-nim c -o:base64_nim_clang -d:danger --cc:clang --verbosity:0 test.nim
-cargo build --manifest-path base64.rs/Cargo.toml --release && cp ./base64.rs/target/release/base64 ./base64_rs
-mcs -debug- -optimize+ test.cs
-dotnet build -c Release
+echo building crystal
+time crystal build test.cr --release -o base64_cr --no-debug
+echo building go
+time go build -o base64_go test.go
+echo building go gcc
+time gccgo -O3 -g -o base64_go_gccgo test.go
+echo building c++
+time g++ -O3 -o base64_cpp test.cpp -lcrypto -I../common/libnotify -L../common/libnotify -lnotify
+echo building C
+time gcc -O3 -o base64_c test.c -I../common/libnotify -L../common/libnotify -lnotify
+echo building scala
+time scalac test.scala
+echo building java
+time javac Base64Java.java
+echo building kotlin
+time kotlinc Test.kt -include-runtime -jvm-target 13 -d Test-kt.jar
+echo building D
+time dmd -ofbase64_d -O -release -inline test.d
+echo building D gdc
+time gdc -o base64_d_gdc -O3 -frelease -finline test.d
+echo building ???
+time ldc2 -ofbase64_d_ldc -O5 -release test.d
+echo building nim gcc
+time nim c -o:base64_nim_gcc -d:danger --cc:gcc --verbosity:0 test.nim
+echo building nim clang
+time nim c -o:base64_nim_clang -d:danger --cc:clang --verbosity:0 test.nim
+echo building rust
+time cargo build --manifest-path base64.rs/Cargo.toml --release && cp ./base64.rs/target/release/base64 ./base64_rs
+echo building C# mono
+time mcs -debug- -optimize+ test.cs
+echo building C# dotnet
+time dotnet build -c Release
 
 if [ ! -d aklomp-base64 ]; then
   git clone --depth 1 https://github.com/aklomp/base64.git aklomp-base64
@@ -29,5 +45,7 @@ if [ ! -d aklomp-base64 ]; then
 fi
 gcc -O3 test-aklomp.c -I aklomp-base64/include/ aklomp-base64/lib/libbase64.o -o base64_c_ak -I../common/libnotify -L../common/libnotify -lnotify
 wget -qO - https://cpanmin.us | perl - -L perllib MIME::Base64::Perl
-v -prod -cc gcc -o base64_v_gcc test.v
-v -prod -cc clang -o base64_v_clang test.v
+echo building V gcc
+time v -prod -cc gcc -o base64_v_gcc test.v
+echo building V clang
+time v -prod -cc clang -o base64_v_clang test.v
---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+docker build -t language-bench-debian/perf -f Dockerfile.debian.perf .
+docker build -t language-bench-nix/perf -f Dockerfile.nix.perf .
+
+docker build -t language-bench-nix/go -f ./docker/Dockerfile.nix.go.docker .
+docker build -t language-bench-nix/nim -f ./docker/Dockerfile.nix.nim.docker .
+
+
